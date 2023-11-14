0815ec041567d1f9884bef5cdeb4a3fbd9cd6fe8
Gabriela Gonzaga <gabrielamgonzaga1@gmail.com>
2023-11-14 19:20:01 -0300

    chore: vercel configs

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (_req: Request, res: Response) => {
-  return res.send('Express Typescript on Vercel')
+	return res.send('Express Typescript on Vercel')
 })
 
 app.get('/ping', (_req: Request, res: Response) => {
-  return res.send('pong 🏓')
+	return res.send('pong 🏓')
 })
 
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`)
+	return console.log(`Server is listening on ${port}`)
 })
